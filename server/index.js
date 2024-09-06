8791b5737aaebe111937bb326cf7c59d02629579
HassanKhan8264 <HF610061@GMAIL.COM>
2024-09-06 21:40:38 +0500

    cors

--- server/index.js
+++ server/index.js
@@ -1,32 +1,34 @@
 const express = require("express");
-const cors = require("cors");
 const currencyRouter = require("./currencyController");
 const config = require("./config");
 
 const app = express();
+const cors = require("cors");
 
-// Define allowed origins
-const allowedOrigins = [
-  "http://localhost:4200",
-  "https://currency-converter-eta-seven.vercel.app",
-  "https://currency-converter-qaze.vercel.app"
-];
+app.use(cors())
 
-// Use CORS middleware
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    }
-  })
-);
+// // Define allowed origins
+// const allowedOrigins = [
+//   "http://localhost:4200",
+//   "https://currency-converter-eta-seven.vercel.app",
+//   "https://currency-converter-qaze.vercel.app"
+// ];
+
+// // Use CORS middleware
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // Allow requests with no origin (like mobile apps or curl requests)
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         const msg =
+//           "The CORS policy for this site does not allow access from the specified Origin.";
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     }
+//   })
+// );
 
 // Use the currency router
 app.use("/", currencyRouter);
