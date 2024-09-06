2cbf6e680403641fbce54e4935ab89509c590770
HassanKhan8264 <HF610061@GMAIL.COM>
2024-09-06 21:30:57 +0500

    cors init

--- server/index.js
+++ server/index.js
@@ -1,21 +1,35 @@
 const express = require("express");
 const cors = require("cors");
 const currencyRouter = require("./currencyController");
-const config = require("./config")
+const config = require("./config");
 
 const app = express();
 
-// app.use(cors());
+// Define allowed origins
+const allowedOrigins = [
+  "http://localhost:4200",
+  "https://currency-converter-eta-seven.vercel.app",
+  "https://currency-converter-qaze.vercel.app"
+];
+
+// Use CORS middleware
 app.use(
   cors({
-    origin: [
-      "http://localhost:4200", 
-      "https://currency-converter-eta-seven.vercel.app",
-      "https://currency-converter-qaze.vercel.app"
-    ],
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
   })
 );
-app.use('/', currencyRouter);
+
+// Use the currency router
+app.use("/", currencyRouter);
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
