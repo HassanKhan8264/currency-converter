254025a8826e56171d1708bf34485d9a7d285501
HassanKhan8264 <HF610061@GMAIL.COM>
2024-09-06 21:11:39 +0500

    update enviroment

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,13 +1,9 @@
-// src/environments/environment.prod.ts
+
 export const environment = {
   appName: "currency_converter",
   production: true,
-  server: {
-    self: {
-      HOST: "https://currency-converter-eta-seven.vercel.app",
-      getUrl() {
-        return `${this.HOST}`;
-      },
-    },
+  HOST: "https://currency-converter-eta-seven.vercel.app",
+  getUrl() {
+    return `${this.HOST}`;
   },
 };
