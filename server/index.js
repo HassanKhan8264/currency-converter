a1cbe5aa32743da0fc703048e4dc226567378982
HassanKhan8264 <HF610061@GMAIL.COM>
2024-09-06 21:54:37 +0500

    change cors code

--- server/index.js
+++ server/index.js
@@ -5,19 +5,19 @@ const config = require("./config");
 const app = express();
 const cors = require('cors')
 
-app.use(cors());
 
 // // Define allowed origins
-const allowedOrigins = [
-  "http://localhost:4200",
-  "https://currency-converter-eta-seven.vercel.app",
-  "https://currency-converter-qaze.vercel.app"
-];
-
-// Use CORS middleware
 app.use(
-  cors(allowedOrigins)
+  cors({
+    origin: [
+      "http://localhost:4200",
+      "https://currency-converter-eta-seven.vercel.app",
+      "https://currency-converter-qaze.vercel.app",
+    ],
+    credentials: true, // Include credentials if needed
+  })
 );
+
 
 // Use the currency router
 app.use("/", currencyRouter);
