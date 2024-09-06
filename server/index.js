a6533f15831d8c0fbf3b1846ab129e4006d10965
HassanKhan8264 <HF610061@GMAIL.COM>
2024-09-06 21:15:40 +0500

    update server

--- server/index.js
+++ server/index.js
@@ -5,7 +5,12 @@ const config = require("./config")
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:4200", "https://currency-converter-eta-seven.vercel.app"],
+  })
+);
 app.use('/', currencyRouter);
 
 app.listen(config.PORT, () => {
