ee988a1adbf0aa11b7dd8f52b94357d7d2088e78
HassanKhan8264 <HF610061@GMAIL.COM>
2024-09-07 21:04:04 +0500

    dddfvcv

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const currencyRouter = require("./currencyController");
-const config = require("./config");
+const currencyRouter = require("./server/currencyController");
+const config = require("./server/config");
 const cors = require("cors");
 const path = require("path");
 const app = express();
@@ -12,7 +12,7 @@ app.use(cors());
 app.use("/", currencyRouter);
 
 // Correct path to Angular application
-const angularAppPath = path.join(__dirname, "../client/dist/currency_converter");
+const angularAppPath = path.join(__dirname, "./client/dist/currency_converter");
 
 // Serve static files from Angular build directory
 app.use(express.static(angularAppPath));
